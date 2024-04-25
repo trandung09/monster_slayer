38897d3f55c34d442bca06af0fcc206f4b543ae5
trandung09 <trandung09082004@gmail.com>
2024-04-25 09:03:21 +0700

    update and fix bug

--- src/main/java/org/game/character/Player.java
+++ src/main/java/org/game/character/Player.java
@@ -183,6 +183,8 @@ public class Player extends Entity {
             int monsterIndex = coChecker.checkCoWithEntity(this, gp.monsters);
             int bossIndex = coChecker.checkCoWithEntity(this, gp.boss);
 
+            System.out.println(bossIndex + " " + monsterIndex);
+
             pickObject(objIndex);
 
             for (Projectiles e : projectiles) {
